e6d930050bfea5f6768271488cb5bdbc8c854348
Gustavo Costa Souza <drt65211@MAC432701-M.local>
2025-05-05 09:14:31 -0300

    feat: Eslint instalation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,6 @@ import { globalStyles } from '../styles/global'
 
 globalStyles()
 
-export default function App({Component, pageProps}: AppProps){
-    return <Component {...pageProps}/>
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
 }
