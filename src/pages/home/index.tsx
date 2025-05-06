822ae1a605411e8853ca16a347e5716fd815a1b4
Gustavo Costa Souza <drt65211@MAC432701-M.local>
2025-05-06 10:14:32 -0300

    feat: visual structure Home

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Heading, Text } from '@ignite-ui/react'
+import * as S from './styles'
+
+import previewImage from '../../assets/app-preview.png'
+import Image from 'next/image'
+export default function Home() {
+  return (
+    <S.Container>
+      <S.Hero>
+        <Heading as="h1" size="4xl">
+          Agendamento descomplicado
+        </Heading>
+        <Text size="xl">
+          Conecte seu calendário e permita que as pessoas marquem agendamentos
+          no seu tempo livre.
+        </Text>
+      </S.Hero>
+
+      <S.Preview>
+        <Image
+          src={previewImage}
+          height={400}
+          quality={100}
+          priority
+          alt="Calendário simbolizando aplicação em funcionamento"
+        />
+      </S.Preview>
+    </S.Container>
+  )
+}
