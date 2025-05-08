25168598f01717b1cae279c9f74a066bd9aacb9c
Gustavo Costa Souza <drt65211@MAC432701-M.local>
2025-05-08 14:19:53 -0300

    feat: User register - Component: react-hook-form utility

--- src/pages/home/components/ClaimUsernameForm/index.tsx
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -1,11 +1,31 @@
 import { Button, TextInput } from '@ignite-ui/react'
 import { Form } from './styles'
 
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
 import { ArrowRight } from '@phosphor-icons/react'
+
+const ClaimUsernameFormSchema = z.object({
+  username: z.string(),
+})
+
+type ClaimUsernameFormData = z.infer<typeof ClaimUsernameFormSchema>
 export default function ClaimUsernameForm() {
+  const { register, handleSubmit } = useForm<ClaimUsernameFormData>()
+
+  async function handleClaimUsername(data: ClaimUsernameFormData) {
+    console.log(data)
+  }
+
   return (
-    <Form as="form">
-      <TextInput size="sm" prefix="ignite.com/" placeholder="seu-usuário" />
+    <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
+      <TextInput
+        size="sm"
+        prefix="ignite.com/"
+        placeholder="seu-usuário"
+        {...register('username')}
+      />
       <Button size="sm" type="submit">
         Reservar
         <ArrowRight />
